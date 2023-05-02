b65bba1af03a854a81567f447a588ab2906d2636
Suresh Mohan <smohan@yahoo.com>
2023-05-02 19:41:06 +0000

    changed variables

--- variables.tf
+++ variables.tf
@@ -63,12 +63,12 @@ variable "admin_password" {
 }
 
 variable "height" {
-  default     = "400"
+  default     = "600"
   description = "Image height in pixels."
 }
 
 variable "width" {
-  default     = "600"
+  default     = "400"
   description = "Image width in pixels."
 }
 
